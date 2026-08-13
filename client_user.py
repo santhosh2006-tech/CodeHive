8142ec6d7a7dc908ab6c2713f929d9ac631c1e8c
Santhosh Pulloju <pullojusanthosh0@gmail.com>
2026-08-13 11:09:22 +0530

    Commit by Worker 1

--- client_user.py
+++ client_user.py
@@ -1,1 +1,11 @@
-# Base file placeholder
+from user_db import User, create_db
+from sqlalchemy.orm import sessionmaker
+from logger import logger
+
+def get_user(username):
+    engine = create_db()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    user = session.query(User).filter_by(username=username).first()
+    session.close()
+    return user
--- config.py
+++ config.py
@@ -1,1 +1,7 @@
-# Base file placeholder
+DB_HOST = 'localhost'
+DB_PORT = 5432
+DB_USERNAME = 'username'
+DB_PASSWORD = 'password'
+DB_NAME = 'user_db'
+
+SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
