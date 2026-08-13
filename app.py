b5a2bcaabc974073c883d0b5d46a24c73391959d
Santhosh Pulloju <pullojusanthosh0@gmail.com>
2026-08-13 11:07:01 +0530

    Base for warning test

--- app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-@app.route('/health', methods=['GET'])
-def health_check():
-    return jsonify({'status': 200, 'message': 'Service is healthy'}), 200
