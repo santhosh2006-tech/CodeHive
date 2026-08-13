37666ddbac3bbf23eac332bd77581989a5b898bb
Santhosh Pulloju <pullojusanthosh0@gmail.com>
2026-08-13 18:14:25 +0530

    Commit by Worker 4

--- test_api_gateway.py
+++ test_api_gateway.py
@@ -1,1 +1,18 @@
-# Base file placeholder
+import unittest
+from api_gateway import ApiGateway
+
+class TestApiGateway(unittest.TestCase):
+    def test_create_api(self):
+        api_gateway = ApiGateway()
+        api_id = api_gateway.create_api('example_api', '/example')
+        self assertNotEqual(api_id, None)
+
+    def test_get_api(self):
+        api_gateway = ApiGateway()
+        api_id = api_gateway.create_api('example_api', '/example')
+        api = api_gateway.get_api(api_id)
+        self.assertEqual(api['name'], 'example_api')
+        self.assertEqual(api['path'], '/example')
+
+if __name__ == '__main__':
+    unittest.main()
--- test_service_registry.py
+++ test_service_registry.py
@@ -1,1 +1,18 @@
-# Base file placeholder
+import unittest
+from service_registry import ServiceRegistry
+
+class TestServiceRegistry(unittest.TestCase):
+    def test_register_service(self):
+        service_registry = ServiceRegistry()
+        service_id = service_registry.register_service('example_service', 'http://example.com')
+        self assertNotEqual(service_id, None)
+
+    def test_get_service(self):
+        service_registry = ServiceRegistry()
+        service_id = service_registry.register_service('example_service', 'http://example.com')
+        service = service_registry.get_service(service_id)
+        self.assertEqual(service['name'], 'example_service')
+        self.assertEqual(service['url'], 'http://example.com')
+
+if __name__ == '__main__':
+    unittest.main()
