7ce2a13bf963cddcd80b6c39783137f1d62b7120
Santhosh Pulloju <pullojusanthosh0@gmail.com>
2026-08-13 11:09:23 +0530

    Commit by Worker 2

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
-# Base file placeholder
+import logging
+from logger import Logger
+from service_registry import ServiceRegistry
+
+logger = Logger().get_logger()
+
+REGISTRY_CONFIG = {
+    'host': 'localhost',
+    'port': 9002
+}
+
+registry = ServiceRegistry()
--- service_registry.py
+++ service_registry.py
@@ -1,1 +1,22 @@
-# Base file placeholder
+import logging
+from logger import Logger
+
+logger = Logger().get_logger()
+
+class ServiceRegistry:
+    def __init__(self):
+        self.services = {}
+
+    def register(self, service_name, service_url):
+        self.services[service_name] = service_url
+        logger.info(f'Registered service {service_name} with URL {service_url}')
+
+    def unregister(self, service_name):
+        if service_name in self.services:
+            del self.services[service_name]
+            logger.info(f'Unregistered service {service_name}')
+        else:
+            logger.warning(f'Service {service_name} is not registered')
+
+    def get_service_url(self, service_name):
+        return self.services.get(service_name)
