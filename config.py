d492b0219b0fae3bc6d3b035be379d37c94c339c
Santhosh Pulloju <pullojusanthosh0@gmail.com>
2026-08-13 11:49:13 +0530

    Commit by Worker 2

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
-# Base file placeholder
+# Registry Configurations
+
+SERVICE_REGISTRY_PORT = 9002
+CLIENT_REGISTRY_PORT = 9002
+
+SERVICE_REGISTRY_URL = f'http://localhost:{SERVICE_REGISTRY_PORT}/service-registry'
+CLIENT_REGISTRY_URL = f'http://localhost:{CLIENT_REGISTRY_PORT}/client-registry'
