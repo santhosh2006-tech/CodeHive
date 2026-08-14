b204b5be1e208679c47c01e1b634f6dc16553a8c
Santhosh Pulloju <pullojusanthosh0@gmail.com>
2026-08-14 10:59:26 +0530

    Commit by Worker 1

--- client_user.py
+++ client_user.py
@@ -1,1 +1,17 @@
-# Base file placeholder
+from user_db import UserDB
+
+class ClientUser:
+    def __init__(self, db_name):
+        self.user_db = UserDB(db_name)
+
+    def register(self, username, password):
+        if self.user_db.get_user(username):
+            return False
+        self.user_db.insert_user(username, password)
+        return True
+
+    def login(self, username, password):
+        user = self.user_db.get_user(username)
+        if user and user[2] == password:
+            return True
+        return False
