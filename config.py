aeeff0786fcaebce4dc1dbb090303039654f9e11
Santhosh Pulloju <pullojusanthosh0@gmail.com>
2026-08-14 10:56:44 +0530

    Commit by Worker 2

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
-# Base file placeholder
+# Registry configurations
+SERVICE_REGISTRY_URL = 'http://localhost:9002'
+CLIENT_REGISTRY_URL = 'http://localhost:9002'
--- service_registry.py
+++ service_registry.py
@@ -1,1 +1,15 @@
-# Base file placeholder
+import os
+
+class ServiceRegistry:
+    def __init__(self):
+        self.services = {}
+
+    def register(self, name, url):
+        self.services[name] = url
+
+    def unregister(self, name):
+        if name in self.services:
+            del self.services[name]
+
+    def get_service(self, name):
+        return self.services.get(name)
