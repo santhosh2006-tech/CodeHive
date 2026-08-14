3898f36c675af87caaf2bbe338aeff4da45d00bb
Santhosh Pulloju <pullojusanthosh0@gmail.com>
2026-08-14 10:38:19 +0530

    Commit by Worker 1

--- client_user.py
+++ client_user.py
@@ -1,1 +1,19 @@
-# Base file placeholder
+from user_db import UserDB
+
+class ClientUser:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.user_db = UserDB()
+
+    def register(self):
+        if self.user_db.get_user(self.username):
+            return False
+        self.user_db.insert_user(self.username, self.password)
+        return True
+
+    def login(self):
+        user = self.user_db.get_user(self.username)
+        if user and user[2] == self.password:
+            return True
+        return False
--- user_db.py
+++ user_db.py
@@ -1,1 +1,26 @@
-# Base file placeholder
+import sqlite3
+from config import DB_NAME
+
+class UserDB:
+    def __init__(self):
+        self.conn = sqlite3.connect(DB_NAME)
+        self.cursor = self.conn.cursor()
+        self.create_table()
+
+    def create_table(self):
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS users
+            (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)
+        """)
+        self.conn.commit()
+
+    def insert_user(self, username, password):
+        self.cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+        self.conn.commit()
+
+    def get_user(self, username):
+        self.cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
+        return self.cursor.fetchone()
+
+    def close(self):
+        self.conn.close()
